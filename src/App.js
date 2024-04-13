6f856cf3ffee6ace1de44e5ce0c903ee7e0b2960
tallman <vedunaik@gmail.com>
2024-04-13 15:13:00 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
-      
-    </div>
+      <div className=''>
+          <h1 className='text-3xl text-red-500 font-bold underline'>Hello world!</h1>
+      </div>
   );
 }
 
